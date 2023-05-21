d9d47cae3ace9cf077e1f5963e4651c04d987097
Gabriel Lacunza <69773735+SlimeB@users.noreply.github.com>
2023-05-21 09:28:47 -0400

    feat: recoleccion de datos de asignacion de visitas

--- src/controllers/AsignarV.js
+++
@@ -1,6 +0,0 @@
-export const AsignarV = (req, res) => {
-    const asistentes = ["jimi", "pepito", "alejandro"]
-    const visitas = ["gorriones 123", "patios 987", "patata 876"]
-    
-    return res.render("AsignarV", {asistentes, visitas})
-}
--- src/controllers/asignar_visita.js
+++ src/controllers/asignar_visita.js
@@ -1,6 +1,46 @@
-export const asignar_visita = (req, res) => {
-    const asistentes = ["jimi", "pepito", "alejandro"]
-    const visitas = ["gorriones 123", "patios 987", "patata 876"]
-    
-    return res.render("asignar_visita", {asistentes, visitas})
+import { connection } from "../database/db.js"
+
+const obtenerAsistentes = async () => {
+    try {
+      const asistentes = await new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM usuario", (err, rows) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+  
+          resolve(rows);
+        });
+      });
+  
+      return asistentes;
+    } catch (error) {
+      console.error('Error al ejecutar la consulta:', error);
+    }
+  };
+
+  const obtenerHogares = async () => {
+    try {
+      const hogares = await new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM hogar", (err, rows) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+  
+          resolve(rows);
+        });
+      });
+  
+      return hogares;
+    } catch (error) {
+      console.error('Error al ejecutar la consulta:', error);
+    }
+  };
+
+export const asignar_visita = async (req, res) => {
+    const asistentes = await obtenerAsistentes()
+    const hogares = await obtenerHogares()
+
+    return res.render("asignar_visita", {asistentes, hogares})
 }
