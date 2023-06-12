66b82643ff4891abdf3305b1807954af67838b40
Diego Gaviria <di.gaviria@duocuc.cl>
2023-06-12 18:15:38 -0400

    editar visitas listo

--- src/controllers/edit_visit.js
+++ src/controllers/edit_visit.js
@@ -2,12 +2,16 @@ import { connection } from "../database/db.js";
 
 export const edit_visit = (req, res) => {
     const { homeId } = req.params;
-    const { rol } = req.user;
     const { jh_name, jh_last_name, jh_rut, jh_phone, jh_email, jh_address, jh_commune } = req.body;
+
 
     connection.query('UPDATE hogar SET  direccion = ?, comuna = ?  WHERE id_hogar = ?',
         [jh_address, jh_commune, homeId]);
-    return res.render("edit_visit", { homeId, rol });
+
+    connection.query('UPDATE jefe_hogar SET  nombre = ?,  apellidos = ?,  rut = ?,  celular = ?,  correo = ? WHERE id_hogar = ?',
+        [jh_name, jh_last_name, jh_rut, jh_phone, jh_email, homeId]);
+
+    return res.redirect(`/visit_history`)
 }
 
 export const edit_visit_render = (req, res) => {
