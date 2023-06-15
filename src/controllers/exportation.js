549310ed1c8c3422f75463f84e5dfa1a3feb525e
Gabriel Lacunza <69773735+SlimeB@users.noreply.github.com>
2023-06-15 16:31:09 -0400

    resumen de reporte agregado

--- src/controllers/exportation.js
+++ src/controllers/exportation.js
@@ -27,6 +27,8 @@ export const exporting = async (req, res) => {
     })
 
     const data = [];
+    
+    
 
     if("all" in req.body){
         data.push(Object.keys(lista_visitas[0]));
@@ -43,6 +45,29 @@ export const exporting = async (req, res) => {
     const worksheet = xlsx.utils.aoa_to_sheet(data)
     const workbook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(workbook, worksheet, "Visistas")
+
+    const resumen = [];
+    const resumen_datos = await new Promise((resolve, reject) => {
+        connection.query('SELECT h.id_hogar "Id_hogar", ROUND(AVG(COALESCE(i.edad , j.edad))) "Edad_promedio", COUNT(i.sexo)+1 "Genero", COUNT(i.nivel_educacional) "Nivel_educacional", COUNT(i.estado_civil)+1 "Estado_civil", COUNT(j.nacionalidad)+1 "Nacionalidad" FROM `hogar` h LEFT JOIN `jefe_hogar` j ON h.id_hogar = j.id_hogar LEFT JOIN `integrante` i ON h.id_hogar = i.id_hogar GROUP BY Id_hogar;', async (err, results) => {
+            if (err) {
+                reject(err);    
+            }
+            resolve(results);
+        })
+    })
+    resumen.push(Object.keys(resumen_datos[0]));
+    if("all" in req.body){
+        resumen_datos.forEach(dato => {
+            resumen.push(Object.values(dato))
+        });
+    }else if ("selected" in req.body) {
+        resumen_datos.forEach(dato => {
+            String(dato.id_hogar) in req.body && resumen.push(Object.values(dato));
+        })
+    }
+    const resumenworksheet = xlsx.utils.aoa_to_sheet(resumen)
+    xlsx.utils.book_append_sheet(workbook, resumenworksheet, "resumen hogares")
+    
     xlsx.writeFile(workbook, "./public/Visitas.xlsx")
 
     return res.render("exporting", {rol, lv: lista_visitas})
